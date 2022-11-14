3cc63b465c31ce399db84c423eb5b47a1c22fef0
Paulo Roberto <89588460+paulorag@users.noreply.github.com>
2022-11-14 09:02:33 -0300

    att

--- src/pages/home/HomePage.js
+++ src/pages/home/HomePage.js
@@ -2,13 +2,21 @@ import { Card } from "../../components/Card/Card"
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-// import { ModalSavePin } from "../../containers/ModalSavePin/ModalSavePin";
+import { ModalSavePin } from "../../containers/ModalSavePin/ModalSavePin";
 import { ModalCreateFolder } from "../../containers/ModalCreateFolder/ModalCreateFolder";
+import { Notification } from "../../components/Notifications/Notification";
 
 export const HomePage = () => {
     return (
         <div>
-            <ModalCreateFolder open={true} />
+            <ModalSavePin open={false} />
+            <ModalCreateFolder open={false} />
+            <Notification
+                message='Criado com sucesso'
+                onClose={() => {
+                    console.log('Clicou')
+                }}
+            />
             <Container fluid>
                 <Row>
                     <Col xs={12} md={2}><Card title="Matematica" imagem="https://picsum.photos/200/300?53" total={0} /></Col>
